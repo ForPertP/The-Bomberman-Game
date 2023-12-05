40f2ee7c13542086213be909c462e0deb796e59e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-12-06 02:08:15 +0900

    Update The-Bomberman-Game.cpp

--- The-Bomberman-Game.cpp
+++ The-Bomberman-Game.cpp
@@ -33,6 +33,46 @@ void detonate(vector<string>& currentGrid, const vector<string>& previousGrid) {
 }
 
 vector<string> bomberMan(int n, vector<string> grid) {
+    vector<string> currentGrid = grid;
+    vector<string> fullyBombedGrid = grid;
+    vector<string> detonationPattern;
+
+    size_t rows = grid.size();
+    size_t cols = grid[0].size();
+
+    // Set all cells to 'O' initially
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            currentGrid[i][j] = 'O';
+
+    // Set all cells to 'O' for fully bombed grid
+    fullyBombedGrid = currentGrid;
+
+    // Detonate initial grid
+    detonate(currentGrid, grid);
+
+    // Save detonation pattern
+    detonationPattern = currentGrid;
+
+    // Set all cells to 'O' for the next detonation
+    for (size_t i = 0; i < rows; ++i)
+        for (size_t j = 0; j < cols; ++j)
+            currentGrid[i][j] = 'O';
+
+    // Detonate the next pattern
+    detonate(currentGrid, detonationPattern);
+
+    int patternNumber = n % 4;
+
+    if (n == 1)
+        return grid;
+
+    if (patternNumber == 0 || patternNumber == 2)
+        return fullyBombedGrid;
+    else if (patternNumber == 3)
+        return detonationPattern;
+
+    return currentGrid;
 }
 
 int main()
