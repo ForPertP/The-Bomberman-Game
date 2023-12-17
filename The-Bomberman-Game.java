e98cf6a1c72c9c6a6754fe4b19370a8e2c230715
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-12-18 05:46:26 +0900

    Update The-Bomberman-Game.java

--- The-Bomberman-Game.java
+++ The-Bomberman-Game.java
@@ -12,6 +12,15 @@ import static java.util.stream.Collectors.toList;
 
 class Result {
 
+    /*
+     * Complete the 'bomberMan' function below.
+     *
+     * The function is expected to return a STRING_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. STRING_ARRAY grid
+     */
+   
    public static void detonate(List<String> currentGrid, List<String> previousGrid) {
         int rows = currentGrid.size();
         int cols = currentGrid.get(0).length();
@@ -33,17 +42,47 @@ class Result {
         }
     }
 
-    
-    /*
-     * Complete the 'bomberMan' function below.
-     *
-     * The function is expected to return a STRING_ARRAY.
-     * The function accepts following parameters:
-     *  1. INTEGER n
-     *  2. STRING_ARRAY grid
-     */
-}
+    public static List<String> bomberMan(int n, List<String> grid) {
+        List<String> currentGrid = new ArrayList<>(grid);
+        List<String> fullyBombedGrid = new ArrayList<>(grid);
+        List<String> detonationPattern;
 
+        int rows = grid.size();
+        int cols = grid.get(0).length();
+
+        // Set all cells to 'O' initially
+        for (int i = 0; i < rows; ++i)
+            currentGrid.set(i, new String(new char[cols]).replace('\0', 'O'));
+
+        // Set all cells to 'O' for fully bombed grid
+        fullyBombedGrid = new ArrayList<>(currentGrid);
+
+        // Detonate initial grid
+        detonate(currentGrid, grid);
+
+        // Save detonation pattern
+        detonationPattern = new ArrayList<>(currentGrid);
+
+        // Set all cells to 'O' for the next detonation
+        for (int i = 0; i < rows; ++i)
+            currentGrid.set(i, new String(new char[cols]).replace('\0', 'O'));
+
+        // Detonate the next pattern
+        detonate(currentGrid, detonationPattern);
+
+        int patternNumber = n % 4;
+
+        if (n == 1)
+            return grid;
+
+        if (patternNumber == 0 || patternNumber == 2)
+            return fullyBombedGrid;
+        else if (patternNumber == 3)
+            return detonationPattern;
+
+        return currentGrid;
+    }    
+}
 
 public class Solution {
     public static void main(String[] args) throws IOException {
